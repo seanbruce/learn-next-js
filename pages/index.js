6c224e251396c162cc7e596b917e005831c8ff88
seanbruceful <seanbruce520@gmail.com>
2020-03-13 15:34:11 +0800

    create components folder to put in all shared components, Next.js actually doesn't care about the folder name

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,9 @@
-import Link from 'next/link';
+import Layout from '../components/Layout';
 
 export default function Index() {
     return (
-        <div>
-            <Link href="/about" title="About Page">
-                <a>About Page</a>
-            </Link>
+        <Layout>
             <p>Hello Next.js</p>
-        </div>
+        </Layout>
     );
 }
