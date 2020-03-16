fa1db1f60e862d042acc320fb9f748f09c279fbe
seanbruceful <seanbruce520@gmail.com>
2020-03-16 10:36:46 +0800

    data fetching in client and server side

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,33 @@
 import Layout from '../components/Layout';
-import PostLink from '../components/PostLink';
+import Link from 'next/link';
+import fetch from 'isomorphic-unfetch';
 
-export default function Index() {
+const Index = props => {
     return (
         <Layout>
-            <p>Hello Next.js</p>
+            <h1>Batman TV Shows</h1>
             <ul>
-                <PostLink id="Hello Next.js" />
-                <PostLink id="Learn Next.js is awesome" />
-                <PostLink id="Deploy apps with Zeit" />
+                {props.shows.map(show => (
+                    <li key={show.id}>
+                        <Link href="/p/[id]" as={`/p/${show.id}`}>
+                            <a>{show.name}</a>
+                        </Link>
+                    </li>
+                ))}
             </ul>
         </Layout>
     );
 }
+
+Index.getInitialProps = async function () {
+    const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+    const data = await res.json();
+
+    console.log(`Show data fetched. Count: ${data.length}`);
+
+    return {
+        shows: data.map(entry => entry.show)
+    };
+}
+
+export default Index;
--- pages/p/[id].js
+++ pages/p/[id].js
@@ -1,12 +1,24 @@
-import { useRouter, Router } from 'next/router';
 import Layout from '../../components/Layout';
+import fetch from 'isomorphic-unfetch';
 
-export default function Post() {
-    const router = useRouter();
+const Post = props => {
     return (
         <Layout>
-            <h1>{router.query.id}</h1>
-            <p>This is the blog post content.</p>
+            <h1>{props.show.name}</h1>
+            <p>{props.show.summary.replace(/<[/]?[pb]>/g, '')}</p>
+            {props.show.image ? <img src={props.show.image.medium} /> : null}
         </Layout>
     )
 }
+
+Post.getInitialProps = async function (context) {
+    const { id } = context.query;
+    const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
+    const show = await res.json();
+
+    console.log(`Fetched show: ${show.name}`);
+
+    return { show };
+}
+
+export default Post;
